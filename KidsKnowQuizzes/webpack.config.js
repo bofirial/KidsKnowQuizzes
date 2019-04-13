8d31f944a942010d6b449c496b8f533f64e72f17
James Schafer <james@jschafer.net>
2019-04-13 19:56:43 -0400

    Add CleanWebpackPlugin and webpack optimization

--- KidsKnowQuizzes/webpack.config.js
+++ KidsKnowQuizzes/webpack.config.js
@@ -2,40 +2,64 @@ const path = require('path');
 var merge = require('extendify')({ isDeep: true, arrays: 'concat' });
 var devConfig = require('./webpack.config.dev');
 var prodConfig = require('./webpack.config.prod');
-var isDevelopment = process.env.ASPNETCORE_ENVIRONMENT === 'Development';
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
-module.exports = merge(
-    {
-        entry: {
-            site: [
-                './wwwroot/ts/site.bootstrap.ts'
-            ],
-            chromeCast: [
-                './wwwroot/ts/chromeCast.bootstrap.ts'
-            ]
-        },
-        devtool: 'source-map',
-        module: {
-            rules: [
-                {
-                    test: /\.ts(x?)$/,
-                    use: {
-                        loader: 'ts-loader',
-                        options: {
-                            transpileOnly: true
+module.exports = env => {
+    var isDeployment = env && env.prod;
+
+    return merge(
+        {
+            entry: {
+                site: [
+                    './wwwroot/ts/site.bootstrap.ts'
+                ],
+                chromeCast: [
+                    './wwwroot/ts/chromeCast.bootstrap.ts'
+                ]
+            },
+            devtool: 'source-map',
+            module: {
+                rules: [
+                    {
+                        test: /\.ts(x?)$/,
+                        use: {
+                            loader: 'ts-loader',
+                            options: {
+                                transpileOnly: true
+                            }
+                        },
+                        exclude: /node_modules/
+                    }]
+            },
+            resolve: {
+                extensions: ['.tsx', '.ts', '.js']
+            },
+            output: {
+                path: path.join(__dirname, 'wwwroot', 'dist'),
+                filename: '[name].js',
+                publicPath: '/dist/'
+            },
+            plugins: [
+                new CleanWebpackPlugin({
+                    verbose: true,
+                    dry: false
+                })],
+            optimization: {
+                splitChunks: {
+                    chunks: 'async',
+                    cacheGroups: {
+                        vendors: {
+                            test: /[\\/]node_modules[\\/]/,
+                            priority: -10
+                        },
+                        default: {
+                            minChunks: 2,
+                            priority: -20,
+                            reuseExistingChunk: true
                         }
-                    },
-                    exclude: /node_modules/
-                }]
+                    }
+                }
+            }
         },
-        resolve: {
-            extensions: ['.tsx', '.ts', '.js']
-        },
-        output: {
-            path: path.join(__dirname, 'wwwroot', 'dist'),
-            filename: '[name].js',
-            publicPath: '/dist/'
-        },
-        plugins: []
-    },
-    isDevelopment ? devConfig : prodConfig);
+        !isDeployment ? devConfig : prodConfig);
+};
