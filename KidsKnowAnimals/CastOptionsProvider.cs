0a3e266d7764bc2448de0a89bf97aa35ee9acb06
James Schafer <james@jschafer.net>
2019-04-12 22:15:23 -0400

    Set up Chromecast Button in Kids Know Animals. Closes #4.

---
+++ KidsKnowAnimals/CastOptionsProvider.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Gms.Cast.Framework;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace KidsKnowQuizzes.KidsKnowAnimals
+{
+    [Register("KidsKnowQuizzes/KidsKnowAnimals/CastOptionsProvider")]
+    public class CastOptionsProvider : Java.Lang.Object, IOptionsProvider
+    {
+        public IList<SessionProvider> GetAdditionalSessionProviders(Context appContext)
+        {
+            return null;
+        }
+
+        public CastOptions GetCastOptions(Context appContext)
+        {
+            return new CastOptions.Builder()
+                .SetReceiverApplicationId(appContext.GetString(Resource.String.app_id))
+                .Build();
+        }
+    }
+}
