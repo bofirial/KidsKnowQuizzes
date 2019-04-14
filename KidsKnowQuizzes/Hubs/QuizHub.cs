fe01c4fef6b6f0c775f4d63e9ff2728ed787b735
James Schafer <james@jschafer.net>
2019-04-14 19:53:43 -0400

    Added SignalR functionality to the chrome cast app. Closes #7

---
+++ KidsKnowQuizzes/Hubs/QuizHub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace KidsKnowQuizzes.Hubs
+{
+    public class QuizHub : Hub
+    {
+        public async Task RegisterTest(string userName)
+        {
+            await Clients.All.SendAsync("test", $"{userName} has joined.");
+        }
+    }
+}
--- KidsKnowQuizzes/wwwroot/ts/chromeCast.ts
+++ KidsKnowQuizzes/wwwroot/ts/chromeCast.ts
@@ -3,8 +3,31 @@
 
 import '../scss/chromeCast.scss';
 
+// @ts-ignore
+import * as signalR from '@aspnet/signalr';
+
 export class ChromeCast {
     static startApplication() {
         console.log("Starting application.");
+
+        const signalRTest: HTMLDivElement = <HTMLDivElement>(document.querySelector("#signalRTest"));
+
+        const connection = new signalR.HubConnectionBuilder()
+            .withUrl("/hubs/quiz")
+            .build();
+
+        connection.start()
+            .catch(err => console.error(err))
+            .then(() => connection.send("registerTest", "ChromeCast"));
+
+        connection.on("test", (message: string) => {
+            let m = document.createElement("li");
+
+            m.innerHTML =
+                `<li class="list-group-item">${message}</li>`;
+
+            signalRTest.appendChild(m);
+            window.scrollTo(0, document.body.scrollHeight);
+        });
     }
 }
